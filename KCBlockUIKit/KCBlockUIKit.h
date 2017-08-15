bc1084233722954d51bd8069318815b957cb4a7d
Karsa Wang <wanghongtao114@gamil.com>
2017-08-15 17:01:35 +0800

    Initial Commit

---
+++ KCBlockUIKit/KCBlockUIKit.h
@@ -0,0 +1,19 @@
+//
+//  KCBlockUIKit.h
+//  KCBlockUIKit
+//
+//  Created by karsa on 2017/8/15.
+//  Copyright © 2017年 karsa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KCBlockUIKit.
+FOUNDATION_EXPORT double KCBlockUIKitVersionNumber;
+
+//! Project version string for KCBlockUIKit.
+FOUNDATION_EXPORT const unsigned char KCBlockUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KCBlockUIKit/PublicHeader.h>
+
+
