9558d618f5848a896d78827fc92b1ecf63827fec
karsa <wanghongtao114@gmail.com>
2018-01-19 17:27:31 +0800

    add kctextviewcontroller

---
+++ KCBlockUIKit/KCTextViewController.swift
@@ -0,0 +1,23 @@
+//
+//  KCTextViewController.swift
+//  KCBlockUIKit
+//
+//  Created by karsa on 2018/1/19.
+//  Copyright © 2018年 karsa. All rights reserved.
+//
+
+import Foundation
+import SnapKit
+
+public class KCTextViewController : KCBlockViewController {
+    public let textView = UITextView()
+    
+    override func loadView() {
+        super.loadView()
+        
+        view.addSubview(textView)
+        textView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+    }
+}
